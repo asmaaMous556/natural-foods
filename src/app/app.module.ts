3113ec8f8d1d9160cf680558b8f71666f4162296
asmaaMous556 <asmaamosmoh@gmail.com>
2022-01-18 13:03:57 +0200

    create the landing and salads pages design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SaladsComponent } from './salads/salads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    SaladsComponent
   ],
   imports: [
     BrowserModule,
